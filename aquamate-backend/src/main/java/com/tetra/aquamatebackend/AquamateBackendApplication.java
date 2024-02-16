6ca27b6f2321278ac4302390903eb8f3fbf80379
Mohammed <mdeeb@sfsu.edu>
2024-02-15 21:23:42 -0800

    adding java doc

---
+++ aquamate-backend/src/main/java/com/tetra/aquamatebackend/AquamateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tetra.aquamatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquamateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquamateBackendApplication.class, args);
+	}
+
+}
---
+++ aquamate-backend/src/test/java/com/tetra/aquamatebackend/AquamateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tetra.aquamatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquamateBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
