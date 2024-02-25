ec52c20bd87b3a1a55b74b377032b3db436c43f7
Mohammed <mdeeb@sfsu.edu>
2024-02-24 20:27:53 -0800

    Set up Spring Application (backend) and connected it to the database)

---
+++ aquamate/src/test/java/me/aquamate/AquamateApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aquamate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquamateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
