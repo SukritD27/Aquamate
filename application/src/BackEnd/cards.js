784d01b1b19503b6e0de76d0028e9d6d1ba38a9c
SukritD27 <sdhawan@sfsu.edu>
2024-03-19 14:59:53 -0700

    Got mongodb query working. Returns all documents containing the search string in the commonName field in fauna collection.

--- application/src/BackEnd/cards.js
+++ application/src/BackEnd/cards.js
@@ -1,31 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const client = require('./config/database.js');
+// const client = require('./config/database.js');
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.DB_URI);
 
-router.get('/', (req, res, next) => {
-    res.json({ message: " passed" }); // Send the message in an object
 
-});
+// router.get('/', (req, res, next) => {
+//     res.json({ message: " passed" }); // Send the message in an object
 
-router.get('/search', async function(req, res, next) {
-    const search = req.query;
-    const client = new MongoClient(process.env.DB_URI);
+// });
+
+router.get('/', async function(req, res, next) {
+    const searchString = req.query;
+    // const searchString = "B";
+    const search = new RegExp(searchString);
     try {
-        const db = client.db(); 
+        const db = client.db("aquamatedb"); 
         const collection = db.collection('fauna'); 
     
-        const searchResults = await collection.find({ commonName: search });
-
-        if(await collection.countDocuments(searchResults) === 0){
-            console.log("No Docs found")
+        const searchResults = await collection.find({ commonName: {$regex: search} });
+        // ${search}
+        if(await collection.countDocuments({ commonName: {$regex: search} }) === 0){
+            console.log('search: ',search);
+            console.log("No Docs found.")
         }else{
+            console.log('search: ',search);
             res.json(searchResults);
         }
         for await (const doc of searchResults) {
             console.dir(doc);
         }
-        
+
       } catch (error) {
         console.error('Error searching MongoDB:', error);
         res.status(500).json({ error: 'Internal Server Error' });
