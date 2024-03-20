dac120be4cdbfb5a7711af069297aabcb09c5d92
Miguel <111917290+mlvargas831@users.noreply.github.com>
2024-03-19 23:31:19 -0700

    WIP
    Front End Search WIP

--- application/src/BackEnd/cards.js
+++ application/src/BackEnd/cards.js
@@ -4,21 +4,21 @@ const mongoose = require('mongoose');
 // const client = require('./config/database.js');
 const { MongoClient } = require('mongodb');
 const client = new MongoClient(process.env.DB_URI);
-
+const querystring = require('node:querystring');
 
 // router.get('/', (req, res, next) => {
 //     res.json({ message: " passed" }); // Send the message in an object
 
 // });
 
-router.get('/', async function(req, res, next) {
+router.get('/search', async function(req, res, next) {
     const searchString = req.query;
     // const searchString = "B";
-    const search = new RegExp(searchString);
+        const search = new RegExp(searchString);
     try {
-        const db = client.db("aquamatedb"); 
-        const collection = db.collection('fauna'); 
-    
+        const db = client.db("aquamatedb");
+        const collection = db.collection('fauna');
+
         const searchResults = await collection.find({ commonName: {$regex: search} });
         // ${search}
         if(await collection.countDocuments({ commonName: {$regex: search} }) === 0){
