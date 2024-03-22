7b28384371497dbaf39e5a75b000e2c0a6d78a22
SukritD27 <sdhawan@sfsu.edu>
2024-03-21 22:32:53 -0700

    CSS for home page

--- application/src/react-app/src/App.js
+++ application/src/react-app/src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './home.js';
-
+import './home.css';
+import './header.css';
 
 
 function App() {
@@ -9,24 +10,26 @@ function App() {
 
   useEffect(() => {
   
-    fetch('http://localhost:3000/test')
+    fetch('http://localhost:8080/test')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(err => console.error("Failed to fetch data from backend:", err));
   }, []);
 
   return (
-    // <div className="App">
-    //   <p>Backend: {message}</p>
-    // </div>
-
-    <BrowserRouter>
     <div>
-      <Routes>
-          <Route path="/" element={<HomePage />} />
-      </Routes>
-    </div>
-  </BrowserRouter>
+        <div className="App">
+          <p>Backend: {message}</p>
+        </div>
+
+      <BrowserRouter>
+      <div>
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  </div>
   );
 }
 
