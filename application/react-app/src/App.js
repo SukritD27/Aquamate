fe577aa928cba814da4c690d5d3c4b9c2ba68b00
MohammedDeeb261 <mdeeb@sfsu.edu>
2024-04-03 13:27:21 -0700

    file organization, added builder_tool dir to react

--- application/react-app/src/App.js
+++ application/react-app/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from './home.js';
-import './home.css';
-import './header.css';
+import './css/home.css';
+import './css/header.css';
 
 
 function App() {
